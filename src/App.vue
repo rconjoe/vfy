89c56de07ce50da244eb46bff28f9ecc0485bb5b
trog <jcarlton1227@gmail.com>
2020-11-04 21:41:06 -0500

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,68 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+      <!-- navbar -->
+      <v-app-bar app color="primary" dark>
+          <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
+          <v-spacer />
+          <v-btn 
+              v-for="link in links"
+              :key="`${link.label}-header-link`"
+              text
+              rounded 
+              :to="link.url"
+              >
+              {{ link.label }}
+          </v-btn>
+      </v-app-bar>
+      <v-content>
+          <router-view />
+      </v-content>
+      <!-- footer -->
+      <v-footer app color="primary lighten-1" padless>
+          <v-layout justify-center wrap>
+              <v-btn
+                  v-for="link in links"
+                  :key="`${link.label}-footer-link`"
+                  color="white"
+                  text
+                  rounded
+                  class="my-2"
+                  :to="link.url"
+              >
+              {{ link.label }}
+              </v-btn>
+              <v-flex primary lighten-2 py-4 text-center white--text xs12>
+                  {{ new Date().getFullYear() }} - <strong>Vuetify Dashboard</strong>
+              </v-flex>
+          </v-layout>
+      </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+      links: [
+          {
+              label: 'Home',
+              url: '/'
+          },
+          {
+              label: 'Login',
+              url: '/login'
+          },
+          {
+              label: 'Signup',
+              url: '/signup'
+          },
+          {
+              label: 'Dashboard',
+              url: '/dashboard'
+          },
+      ] 
+  }),
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/Login.vue'),
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: () => import ('@/views/Dashboard.vue'),
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: () => import('@/views/Signup.vue')
+  },
 ]
 
 const router = new VueRouter({
